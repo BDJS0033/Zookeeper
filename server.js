2dc63b49ae4035d071c27846be115173f61e5fb7
SAB <sevanboyadjian@gmail.com>
2022-03-03 20:23:43 -0500

    adding get/post

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const { animals } = require('./data/animals.json');
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const { animals } = require('./data/animals.json');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -17,14 +25,7 @@ function filterByQuery(query, animalsArray) {
         }
         //Loop through each trait in the personalityTraits array:
         personalityTraitsArray.forEach(trait => {
-            //Check the trait against each animal in the filertedResults array.
-            //Remember, it is initially a copy of the animalsArray,
-            //but here we're updating it for each train in the .forEach() loop.
-            //For each trait being targeted by the filter, the filteredResults
-            //array will then contain only the entries that contain the trait,
-            //so at the end we'll have an array of animals that have every one
-            //of the traits when the .forEach() loop is finished.
-            filteredResults = filteredResults.filter(
+                        filteredResults = filteredResults.filter(
                 animal => animal.personalityTraits.indexOf(trait) !== -1
             );
         });
@@ -47,6 +48,17 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    //return finished code to post route for response
+    return animal;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -55,12 +67,22 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+app.post('/api/animals', (req, res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    //add anima to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animalsArray);
+    res.json(req.body);
+});
+
+
 app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
-    if(result) {
-        res.json(result); 
-    }else{
-        res.send(404);
+    if (result) {
+        res.json(result);
+    } else {
+    res.send(404);
     }
 });
 
