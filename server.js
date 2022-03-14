96d3e26219a9b830d8fd05bebe6025b4a5372366
SAB <sevanboyadjian@gmail.com>
2022-03-14 13:46:31 -0400

    adding validation

--- server.js
+++ server.js
@@ -52,40 +52,57 @@ function createNewAnimal(body, animalsArray) {
     const animal = body;
     animalsArray.push(animal);
     fs.writeFileSync(
-        path.join(__dirname, './data/animals.json'),
-        JSON.stringify({ animals: animalsArray }, null, 2)
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
     );
-    //return finished code to post route for response
     return animal;
-}
-
-app.get('/api/animals', (req, res) => {
+  }
+  
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+  
+  app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
-        results = filterByQuery(req.query, results);
+      results = filterByQuery(req.query, results);
     }
     res.json(results);
-});
-
-app.post('/api/animals', (req, res) => {
-    //set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-
-    //add anima to json file and animals array in this function
-    const animal = createNewAnimal(req.body, animalsArray);
-    res.json(req.body);
-});
-
-
-app.get('/api/animals/:id', (req, res) => {
+  });
+  
+  app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
     if (result) {
-        res.json(result);
+      res.json(result);
     } else {
-    res.send(404);
+      res.send(404);
     }
-});
-
-app.listen(PORT, () => {
+  });
+  
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
+  });
+  app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-});
+  });
+  
